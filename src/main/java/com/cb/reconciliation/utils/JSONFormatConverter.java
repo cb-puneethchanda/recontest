5b56ae617a3155ecbf515c0007fa0b3f27fb8266
cb-hiruthikj <hiruthik.j@chargebee.com>
2022-04-02 13:36:28 +0530

    Declare toSimpleJSON for converting JSON type

--- src/main/java/com/cb/reconciliation/utils/JSONFormatConverter.java
+++ src/main/java/com/cb/reconciliation/utils/JSONFormatConverter.java
@@ -1,5 +1,6 @@
 package com.cb.reconciliation.utils;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -10,6 +11,15 @@ public class JSONFormatConverter {
         Object obj = parser.parse(jsonObject.toString());
         org.json.simple.JSONObject simpleJSONFormat = (org.json.simple.JSONObject) obj;
         return simpleJSONFormat;
+    }
+
+    // todo: fix
+    public static JSONObject toOrgJSON(org.json.simple.JSONObject simpleJsonObject) throws ParseException, JSONException {
+        String data = simpleJsonObject.toJSONString();
+//        JSONObject orgJSON = new JSONObject(data);
+        System.out.println("Data");
+        System.out.println(data);
+        return new JSONObject(data);
     }
 
 }
