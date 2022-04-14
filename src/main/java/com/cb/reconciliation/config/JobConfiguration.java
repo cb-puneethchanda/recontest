94e7708054ede847e00b1446d7eb8134c80ba5bc
cb-sushank <sushank.sakthivel@chargebee.com>
2022-04-14 16:32:19 +0530

    hardcoded db removed

--- src/main/java/com/cb/reconciliation/config/JobConfiguration.java
+++ src/main/java/com/cb/reconciliation/config/JobConfiguration.java
@@ -18,47 +18,7 @@ public class JobConfiguration {
     @Bean
     CommandLineRunner commandLineRunner(JobRepository repository) {
         return args -> {
-            JSONObject json = new JSONObject("{\n" +
-                    "    \"mismatched\": [\n" +
-                    "        {\n" +
-                    "            \"date\": \"2022-03-30T21:06:22\",\n" +
-                    "            \"transactionType\": \"refund\",\n" +
-                    "            \"amount\": 29900.0,\n" +
-                    "            \"id\": \"re_3Kj0geSFiiJc1ZKR0w3iP1Us\",\n" +
-                    "            \"currencyCode\": \"INR\",\n" +
-                    "            \"issues\": \"NONE\"\n" +
-                    "        }\n" +
-                    "    ],\n" +
-                    "    \"metadata\": {}\n" +
-                    "}");
-
-            Job newJob = Job.builder()
-//                    .jobId(UUID.randomUUID().toString())
-                    .jobId("1")
-                    .chargebeeSiteUrl("url1")
-                    .gateway("stripe")
-                    .startTime(Timestamp.valueOf(LocalDateTime.of(2022, 3, 22, 0, 0)))
-                    .endTime((Timestamp.valueOf(LocalDateTime.now())))
-                    .createdAt(Timestamp.valueOf(LocalDateTime.now()))
-                    .status("PROCESSING")
-                    .mismatched(json)
-                    .build();
-
-            Job newJob2 = Job.builder()
-//                    .jobId(UUID.randomUUID().toString())
-                    .jobId("2")
-                    .chargebeeSiteUrl("url1")
-                    .gateway("stripe")
-                    .startTime(Timestamp.valueOf(LocalDateTime.of(2022, 3, 22, 0, 0)))
-                    .endTime((Timestamp.valueOf(LocalDateTime.now())))
-                    .createdAt(Timestamp.valueOf(LocalDateTime.now()))
-                    .status("SUCCESS")
-                    .mismatched(json)
-                    .build();
-
             List<Job> l = new ArrayList<Job>();
-            l.add(newJob);
-            l.add(newJob2);
             repository.saveAll(l);
         };
     }
